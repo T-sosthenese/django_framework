dce898e494ee561698d213ff5b831ec78292e4a9
sosthene <sosthenetimi94@gmail.com>
2023-08-30 21:52:59 +0300

    More tests

--- hello_world/pages/tests.py
+++ hello_world/pages/tests.py
@@ -1,4 +1,5 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 class HomepageTests(SimpleTestCase):
     """Testing out homepage."""
@@ -6,9 +7,17 @@ class HomepageTests(SimpleTestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
 
 class AboutpageTests(SimpleTestCase):
     """Testing our about page."""
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
