fc39ab6ff1593dcfd61cf1bb32bec4a856deb163
sosthene <sosthenetimi94@gmail.com>
2023-08-31 00:09:26 +0300

    Deployed my posts online via localhost

--- message_board/posts/models.py
+++ message_board/posts/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Post(models.Model):
     """A class for our posts."""
     text = models.TextField()
+
+    def __str__(self):
+        """Returns a string representation of the post."""
+        return self.text[:50] + '...'
--- message_board/posts/views.py
+++ message_board/posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+class HomePageView(ListView):
+    """Custom view our homepage content."""
+    model = Post
+    template_name = "home.html"
