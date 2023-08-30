c6fbfe0c6358a5d1641dd89c245bb7e748d299a5
sosthene <sosthenetimi94@gmail.com>
2023-08-30 22:14:27 +0300

    more tests

--- hello_world/pages/tests.py
+++ hello_world/pages/tests.py
@@ -11,6 +11,14 @@ class HomepageTests(SimpleTestCase):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
 
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Homepage</h1>")
+
 
 class AboutpageTests(SimpleTestCase):
     """Testing our about page."""
@@ -21,3 +29,11 @@ class AboutpageTests(SimpleTestCase):
     def test_url_available_by_name(self):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About page</h1>")
