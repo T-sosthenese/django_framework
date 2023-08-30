b12cd9264a3195cc67fdb73e4eee616648c01be2
sosthene <sosthenetimi94@gmail.com>
2023-08-30 20:50:53 +0300

    adding an "about page"

--- hello_world/pages/views.py
+++ hello_world/pages/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     """A homepage class that inherits from django's TemplateView class."""
     template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    """Creating a template for about page."""
+    template_name = "about.html"
