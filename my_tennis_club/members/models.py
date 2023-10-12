5027f1a23a87f03862bcbdfa801f73198263381f
sosthene <sosthenetimi94@gmail.com>
2023-10-13 01:58:02 +0300

    displaying data from the database

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(default=date.today)
