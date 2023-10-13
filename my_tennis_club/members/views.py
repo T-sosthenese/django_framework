c26843721fe7c9ad87c8b34dd98d800b7b807dcc
sosthene <sosthenetimi94@gmail.com>
2023-10-13 17:20:11 +0300

    order by firstname

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -4,7 +4,7 @@ from .models import Member
 
 
 def members(request):
-    mymembers = Member.objects.all().values()
+    mymembers = Member.objects.all().order_by('firstname').values()
     template = loader.get_template('all_members.html')
     context = {
         'mymembers': mymembers,
