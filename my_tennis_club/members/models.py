fc851a0fba8b1e3045b844da6b8f8ae6044ba512
sosthene <sosthenetimi94@gmail.com>
2023-10-13 15:43:03 +0300

    Altering the list display of our members in admin.py

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -7,8 +7,3 @@ class Member(models.Model):
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(default=date.today)
-
-
-    def __str__(self):
-        """Improving the string representation of our model"""
-        return "{} {}".format(self.firstname, self.lastname)
